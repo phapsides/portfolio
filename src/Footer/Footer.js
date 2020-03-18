084cb4c28c6e89c87b4114397238128b35546b31
Panos Hapsides <p.hapsides@live.com>
2020-03-18 15:42:39 +0000

    Create footer component

---
+++ src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.scss';
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="container">
+                <section className="grid">
+                    <div className="column-xs-12">
+                        <p className="copyright">&copy; Copyright 2020 Panos Hapsides</p>
+                    </div>
+                </section>
+            </div>
+        </footer>
+    )
+}
