1beef1f22c246a36963b0fda4b9d0207392885e5
Panos Hapsides <p.hapsides@live.com>
2020-03-26 23:03:45 +0000

    Introduce animations

--- src/About/About.js
+++ src/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'animate.css';
 import './About.scss';
 import me from './assets/me.jpg';
 import workstation from './assets/workstation.jpg';
@@ -10,16 +11,16 @@ export default function About() {
                 <section class="grid info">
                     <div class="column-xs-12 column-md-1">
                         <div class="about">
-                            <h1 class="section-heading">About Me</h1>
+                            <h1 class="section-heading animated fadeIn delay-2s">About Me</h1>
                         </div>
                     </div>
                     <div class="column-xs-12 column-md-4">
-                        <img className="imageCover" src={me} alt="" />
+                        <img className="imageCover animated fadeInLeft delay-1s" src={me} alt="" />
                     </div>
                     <div class="column-xs-12 column-md-7">
                         <div class="intro">
                             <h2>
-                                I am ...
+                                Front End Web Developer 
                             </h2>
                             <p>
                                 Qui nisi aliqua voluptate ea voluptate nulla sit. Ut sunt nostrud dolor laborum culpa et culpa adipisicing elit voluptate reprehenderit dolore. Id nisi officia culpa enim mollit culpa culpa duis in. Pariatur aute amet magna aute id. Ex minim aliqua ea irure mollit culpa. Nisi esse amet ea reprehenderit adipisicing id voluptate.
@@ -35,12 +36,12 @@ export default function About() {
                     <div class="column-xs-12 column-md-7">
                         <blockquote>
                             <h3>
-                                Qui nisi aliqua voluptate ea voluptate nulla sit. Ut sunt nostrud dolor laborum culpa et culpa adipisicing elit voluptate reprehenderit dolore. Id nisi officia culpa enim mollit culpa culpa duis in. Pariatur aute amet magna aute id. Ex minim aliqua ea irure mollit culpa. Nisi esse amet ea reprehenderit adipisicing id voluptate.
+                                I am drowning in data, but I am starving for knowledge!
                             </h3>
                         </blockquote>
                     </div>
                     <div class="column-xs-12 column-md-5">
-                        <img className="imageCover" src={workstation} alt="" />
+                        <img className="imageCover animated fadeInUp delay-2s" src={workstation} alt="" />
                     </div>
                 </section>
             </div>
