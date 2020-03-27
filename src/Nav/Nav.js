fed509813516cd478f541ef5bf692dd41e0669d4
Panos Hapsides <p.hapsides@live.com>
2020-03-27 15:59:24 +0000

    Add personal logo

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'animate.css';
-// import Logo from './logo.svg';
+import Logo from './logo.svg';
 import './Nav.scss';
 
 export default function Nav() {
@@ -9,8 +9,8 @@ export default function Nav() {
             <div className="container">
                 <div className="grid">
                     <div className="column-xs-12 column-md-8">
-                        <p className="logo animated flipInX delay-3s">Panos Hapsides</p>
-                        {/* <img className="logo" src={Logo} alt="" /> */}
+                        {/* <p className="logo animated flipInX delay-3s">Panos Hapsides</p> */}
+                        <img className="logo animated flipInX delay-3s" src={Logo} alt="" />
                     </div>
                     <div className="column-xs-12 column-md-4">
                         <ul>
