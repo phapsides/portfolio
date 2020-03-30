2de37ae885ac50a1bc0aa235ef48372caddccdef
Panos Hapsides <p.hapsides@live.com>
2020-03-30 17:48:54 +0100

    Create front end data source

---
+++ src/Projects/getProjects.js
@@ -0,0 +1,10 @@
+export default function getProjects() {
+    return [
+        { slug: 'project-01', title: 'Kodflix', image: '' },
+        { slug: 'project-02', title: 'Project 2', image: '' },
+        { slug: 'project-03', title: 'Project 3', image: '' },
+        { slug: 'project-04', title: 'Project 4', image: '' },
+        { slug: 'project-05', title: 'Project 5', image: '' },
+        { slug: 'project-06', title: 'Project 6', image: '' }
+    ];
+}
