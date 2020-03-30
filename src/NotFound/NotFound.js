30c6aeec881bceb23192ebb0d1ed78e32986a591
Panos Hapsides <p.hapsides@live.com>
2020-03-30 17:48:29 +0100

    Style not found page

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './NotFound.scss';
+import Title from './../Title/Title';
 
 export default function NotFound() {
     return (
-        <div className="NotFound">
-            <h1>Ooops, it looks like this page doesn't exist.</h1>
-        </div>
+        <main className="NotFound">
+            <div className="container">
+                <section className="grid">
+                    <div className="column-xs-12 column-md-1">
+                        <Title pageTitle="Not Found"/>
+                    </div>
+                    <div className="column-xs-12 column-md-11">
+                        <div className="messageWrapper">
+                            <div className="message">
+                                <h1>Oops!</h1>
+                                <h2>404 - The Page can't be found</h2>
+                            </div>
+                            <a href="/">Go To Homepage</a>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </main>
     )
 }
