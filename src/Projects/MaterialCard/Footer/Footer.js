57ef789a73c8efacb9c0d572c1e4a2d42e7ed8cc
Panos Hapsides <p.hapsides@live.com>
2020-04-03 13:43:35 +0100

    Use devicons

--- src/Projects/MaterialCard/Footer/Footer.js
+++ src/Projects/MaterialCard/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.scss';
+import 'devicon';
 
 export default function Footer() {
     return (
@@ -7,10 +8,10 @@ export default function Footer() {
             <h4>
                 Technologies/Stack
             </h4>   
-            <i className="fa fa-bars"></i>
-            <i className="fa fa-bars"></i>
-            <i className="fa fa-bars"></i>
-            <i className="fa fa-bars"></i>
+            <i class="devicon devicon-react-original"></i>
+            <i class="devicon devicon-javascript-plain"></i>
+            <i class="devicon devicon-html5-plain"></i>
+            <i class="devicon devicon-css3-plain"></i>
         </div>
     )
 }
