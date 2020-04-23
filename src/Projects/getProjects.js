5ee0e8d94fa1db0174f579e194e0901dc2895a99
Panos Hapsides <p.hapsides@live.com>
2020-04-23 23:54:03 +0300

    Enhance project page

--- src/Projects/getProjects.js
+++ src/Projects/getProjects.js
@@ -1,10 +1,82 @@
 export default function getProjects() {
     return [
-        { slug: 'project-01', title: 'Kodflix', image: '' },
-        { slug: 'project-02', title: 'Project 2', image: '' },
-        { slug: 'project-03', title: 'Project 3', image: '' },
-        { slug: 'project-04', title: 'Project 4', image: '' },
-        { slug: 'project-05', title: 'Project 5', image: '' },
-        { slug: 'project-06', title: 'Project 6', image: '' }
+        { 
+            slug: 'kodflix', 
+            title: 'Kodflix', 
+            image: '',
+            coreTechnologies: [
+                'react-original-wordmark',
+                'javascript-plain',
+                'sass-original',
+                'nodejs-plain'
+            ]
+        },
+        { 
+            slug: 'present-agency', 
+            title: 'PRESENT Agency', 
+            image: '',
+            coreTechnologies: [
+                'html5-plain-wordmark',
+                'css3-plain-wordmark',
+                'jquery-plain-wordmark',
+                'gulp-plain'
+            ]
+        },
+        { 
+            slug: 'hackett', 
+            title: 'Hackett', 
+            image: '',
+            coreTechnologies: [
+                'html5-plain-wordmark',
+                'css3-plain-wordmark',
+            ]
+        },
+        { 
+            slug: 'fidelity-intl', 
+            title: 'Fidelity International', 
+            image: '',
+            coreTechnologies: [
+                'html5-plain-wordmark',
+                'css3-plain-wordmark',
+                'javascript-plain',
+                'foundation-plain-wordmark',
+                'django-plain-wordmark'
+            ]
+        },
+        { 
+            slug: 'currencies-direct', 
+            title: 'Currencies Direct', 
+            image: '',
+            coreTechnologies: [
+                'html5-plain-wordmark',
+                'css3-plain-wordmark',
+                'javascript-plain',
+                'foundation-plain-wordmark',
+                'django-plain-wordmark'
+            ]
+        },
+        { 
+            slug: 'optime-group', 
+            title: 'Optime Group', 
+            image: '',
+            coreTechnologies: [
+                'html5-plain-wordmark',
+                'css3-plain-wordmark',
+                'sass-original',
+                'jquery-plain-wordmark',
+                'bower-plain-wordmark',
+            ]
+        },
+        { 
+            slug: 'name-your-price', 
+            title: 'Name your price', 
+            image: '',
+            coreTechnologies: [
+                'html5-plain-wordmark',
+                'css3-plain-wordmark',
+                'sass-original',
+                'jquery-plain-wordmark'
+            ]
+        }
     ];
 }
